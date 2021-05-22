a3c43bcbf9e015cd3131a3d085c51506aaca345b
Peter Ly <peter.wt.ly@gmail.com>
2021-05-22 15:59:04 -0700

    added logic to tmpenv.sh to resume first then clone repo if not available

--- tmpenv.sh
+++ tmpenv.sh
@@ -1,25 +1,28 @@
-# configure /tmp/tmux/fish portable environment
+# configure a portable tmux/fish environment without installing packages
+
 set -e
 
 export ARCH=$(uname -m)
 export TMPENV="/tmp/.$(whoami)"
 export DF=$TMPENV/tmpenv
+export PATH=$TMPENV/bin:$PATH
+
+[ -x $TMPENV/bin/tmux ] && tmux -2 attach && exit
+
 [ ! -d $TMPENV ] && mkdir $TMPENV
 cd $TMPENV
 
-if grep -q CentOS-7 /etc/os-release; then
-	export FISH="$DF"/bin/fish
-fi
-if grep -q CentOS-8 /etc/os-release; then
-	export FISH="$DF"/bin/fish
-fi
-if grep -q 18.04    /etc/os-release; then
-	export FISH="$DF"/bin/fish
-fi
+#if grep -q CentOS-7 /etc/os-release; then
+#fi
+#if grep -q CentOS-8 /etc/os-release; then
+#fi
+#if grep -q 18.04    /etc/os-release; then
+#fi
 if grep -q 20.04    /etc/os-release; then
-	# Ubuntu 2004
+	echo Ubuntu 2004 $ARCH detected
 	BINPATH=$TMPENV/bin-ubuntu2004-$ARCH/usr/local/bin
 	if [ ! -d $BINPATH ] ; then
+		echo git clone https://github.com/pl643/bin-ubuntu2004-$ARCH
 		git clone https://github.com/pl643/bin-ubuntu2004-$ARCH
 		if [ -d $TMPENV/bin-ubuntu2004-$ARCH/usr/local/bin/ ] ; then
 			ln -sf $TMPENV/bin-ubuntu2004-$ARCH/usr/local/bin $TMPENV/bin
@@ -28,7 +31,5 @@ if grep -q 20.04    /etc/os-release; then
 	if [ ! -d $DF ] ; then
 		git clone https://pl643:Kao95843@github.com/pl643/DF
 	fi
-	PATH=$TMPENV/bin:$PATH
-	export PATH
 	tmux -f $DF/tmux.conf -2 new fish -C "source $DF/fishrc"
 fi
