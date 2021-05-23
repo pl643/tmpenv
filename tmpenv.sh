56af6154d8fc8760df1324f9d475d5dd7d6bc3f2
Your Name <you@example.com>
2021-05-23 06:51:43 -0700

    added ubuntu 16.04 case to tmpenv.sh

--- tmpenv.sh
+++ tmpenv.sh
@@ -11,13 +11,30 @@ export PATH=$TMPENV/bin:$PATH
 
 [ ! -d $TMPENV ] && mkdir $TMPENV
 cd $TMPENV
-
+BINPATH=$TMPENV/bin-ubuntu1604-$ARCH/usr/local/bin
+tmux -f $DF/tmux.conf -2 new fish -C "source $DF/fishrc"
+exit
 #if grep -q CentOS-7 /etc/os-release; then
 #fi
 #if grep -q CentOS-8 /etc/os-release; then
 #fi
 #if grep -q 18.04    /etc/os-release; then
 #fi
+if grep -q 16.04    /etc/os-release; then
+	echo Ubuntu 1604 $ARCH detected
+	BINPATH=$TMPENV/bin-ubuntu1604-$ARCH/usr/local/bin
+	if [ ! -d $BINPATH ] ; then
+		echo git clone https://github.com/pl643/bin-ubuntu2004-$ARCH
+		git clone https://github.com/pl643/bin-ubuntu1604-$ARCH
+		if [ -d $TMPENV/bin-ubuntu1604-$ARCH/usr/local/bin/ ] ; then
+			ln -sf $TMPENV/bin-ubuntu1604-$ARCH/usr/local/bin $TMPENV/bin
+		fi
+	fi
+	if [ ! -d $DF ] ; then
+		git clone https://pl643:Kao95843@github.com/pl643/tmpenv
+	fi
+	tmux -f $DF/tmux.conf -2 new fish -C "source $DF/fishrc"
+fi
 if grep -q 20.04    /etc/os-release; then
 	echo Ubuntu 2004 $ARCH detected
 	BINPATH=$TMPENV/bin-ubuntu2004-$ARCH/usr/local/bin
