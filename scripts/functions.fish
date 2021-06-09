625b3007ad617468ebf7a9c54fd3da6d5f1aa4e7
Peter Ly <peter.wt.ly@gmail.com>
2021-06-08 22:48:45 -0700

    add logic to select current window after window is sorted

--- scripts/functions.fish
+++ scripts/functions.fish
@@ -1,3 +1,5 @@
+set active_pane (tmux list-panes | grep active | cut -d " " -f 7)
+
 function sort-tmux-windows
     while not perl $DF/scripts/tmux-sort-windows.pl; end
 end
@@ -5,3 +7,6 @@ end
 if test -n "$argv"
     $argv
 end
+
+set old_active_window (tmux list-panes -a | grep $active_pane | cut -c 3)
+tmux select-window -t $old_active_window
