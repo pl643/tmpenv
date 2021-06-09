f53a837ca9cb3da9a51b65eeadf67f0241d8c97a
Peter Ly <peter.wt.ly@gmail.com>
2021-06-09 15:55:46 -0700

    moved code to select current active window inside sort-tmux-window function

--- scripts/functions.fish
+++ scripts/functions.fish
@@ -1,12 +1,11 @@
-set active_pane (tmux list-panes | grep active | cut -d " " -f 7)
-
 function sort-tmux-windows
+    set active_pane (tmux list-panes | grep active | cut -d " " -f 7)
     while not perl $DF/scripts/tmux-sort-windows.pl; end
+    set old_active_window (tmux list-panes -a | grep $active_pane | cut -c 3)
+    tmux select-window -t $old_active_window
 end
 
 if test -n "$argv"
     $argv
 end
 
-set old_active_window (tmux list-panes -a | grep $active_pane | cut -c 3)
-tmux select-window -t $old_active_window
