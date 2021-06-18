da123414273941ed0d6b50e4d59ec834ee6c57ec
Peter Ly <peter.wt.ly@gmail.com>
2021-06-18 15:13:31 -0700

    changed M-l M-h to select pre/next pane

--- scripts/toggle_alt.fish
+++ scripts/toggle_alt.fish
@@ -3,8 +3,8 @@
 function toggle_alt_status
     set -x ALTSTAT (tmux showenv TMUX_ALT_HJKL|cut -f2 -d=)
     if test $ALTSTAT -eq 0    # currently off run on script
-       tmux bind-key -n M-h select-pane -L \; bind-key -n  M-j select-pane -D \; bind-key -n M-k select-pane -U \; bind-key -n  M-k send-keys Up \; bind-key -n  M-j send-keys Down \; \
-            bind-key -n  M-l select-pane -R \; set -g prefix C-a \; bind-key -n M-o next-window \; bind-key -n  M-i previous-window \; bind-key -n  M-u swap-pane -U \; bind-key -n M-z resize-pane -Z \; \
+       tmux bind-key -n M-h select-pane -t :.- \; bind-key -n  M-j select-pane -D \; bind-key -n M-k select-pane -U \; bind-key -n  M-k send-keys Up \; bind-key -n  M-j send-keys Down \; \
+            bind-key -n M-l select-pane -t :.+ \; set -g prefix C-a \; bind-key -n M-o next-window \; bind-key -n  M-i previous-window \; bind-key -n  M-u swap-pane -U \; bind-key -n M-z resize-pane -Z \; \
         set-option -g status-right "ALT #[bg=colour237,fg=colour239 nobold, nounderscore, noitalics]#[bg=colour239,fg=colour246] %Y-%m-%d  %H:%M #[bg=colour239,fg=colour248,nobold,noitalics,nounderscore]#[bg=colour248,fg=colour237] #h "
         tmux setenv TMUX_ALT_HJKL 1
     else 
